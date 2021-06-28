992ff66011ced3204484160af4537873adaec5e0
Kyung <kkj3853@gmail.com>
2021-06-28 20:53:45 +0900

    added ROS Communication files

---
+++ ROS_COMM/image_transport_ws/build/learning_image_transport/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kyung/image_transport_ws/src/learning_image_transport
+# Build directory: /home/kyung/image_transport_ws/build/learning_image_transport
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
