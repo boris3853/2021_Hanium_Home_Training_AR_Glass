638550004ef0619b4c7e6907a5bd88939cc71e5e
Kyung <kkj3853@gmail.com>
2021-07-03 17:30:40 +0900

    worked on Yoga-Slim

--- ROS_COMM/transport_ws/src/pub/src/p.cpp
+++ ROS_COMM/transport_ws/src/pub/src/p.cpp
@@ -2,6 +2,7 @@
 #include <image_transport/image_transport.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
+#include <sstream>
 
 int main(int argc, char** argv)
 {
@@ -10,14 +11,17 @@ int main(int argc, char** argv)
   image_transport::ImageTransport it(nh);
   image_transport::Publisher pub = it.advertise("camera/image", 1);
 
-  cv::Mat image = cv::imread(argv[1], cv::IMREAD_COLOR);
-  sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg();
 
-  ros::Rate loop_rate(5);
-  while (nh.ok()) {
-    pub.publish(msg);
-    ros::spinOnce();
-    loop_rate.sleep();
+  static int cnt=1;
+  while(cnt){
+	std::stringstream sstream;
+       	sstream << "image/image" << cnt << ".png";
+  	cv::Mat image = cv::imread(sstream.str(), cv::IMREAD_COLOR);
+  	sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg();
+
+   	pub.publish(msg);
+   	ros::spinOnce();
+   	++cnt;
+	if(cnt == 12) cnt = 1;
   }
 }
-
