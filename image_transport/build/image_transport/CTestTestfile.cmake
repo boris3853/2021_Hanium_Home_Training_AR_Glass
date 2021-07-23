e8a21210a49dedd8e61748de1fa21f45477f4f4d
Kyung <kkj3853@gmail.com>
2021-07-23 15:45:31 +0900

    before compiling

--- image_transport/build/image_transport/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kyung/21_hf271/RPI/image_transport/src/image_transport
-# Build directory: /home/kyung/21_hf271/RPI/image_transport/build/image_transport
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
