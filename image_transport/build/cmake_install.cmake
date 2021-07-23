dbcb3be5f50e46d833d44c22d4963905e480a7fd
TX2 <TX2>
2021-07-23 06:51:54 +0000

    setting for TX2

--- image_transport/build/cmake_install.cmake
+++ image_transport/build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /home/kyung/21_hf271/RPI/image_transport/src
+# Install script for directory: /home/nvidia/21_hf271/image_transport/src
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/home/kyung/21_hf271/RPI/image_transport/install")
+  set(CMAKE_INSTALL_PREFIX "/home/nvidia/21_hf271/image_transport/install")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -32,12 +32,7 @@ if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
   set(CMAKE_INSTALL_SO_NO_EXE "1")
 endif()
 
-# Is this installation the result of a crosscompile?
-if(NOT DEFINED CMAKE_CROSSCOMPILING)
-  set(CMAKE_CROSSCOMPILING "FALSE")
-endif()
-
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
   
       if (NOT EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}")
         file(MAKE_DIRECTORY "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}")
@@ -47,93 +42,93 @@ if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_
       endif()
 endif()
 
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
   list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
-   "/home/kyung/21_hf271/RPI/image_transport/install/_setup_util.py")
+   "/home/nvidia/21_hf271/image_transport/install/_setup_util.py")
   if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
   if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
-file(INSTALL DESTINATION "/home/kyung/21_hf271/RPI/image_transport/install" TYPE PROGRAM FILES "/home/kyung/21_hf271/RPI/image_transport/build/catkin_generated/installspace/_setup_util.py")
+file(INSTALL DESTINATION "/home/nvidia/21_hf271/image_transport/install" TYPE PROGRAM FILES "/home/nvidia/21_hf271/image_transport/build/catkin_generated/installspace/_setup_util.py")
 endif()
 
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
   list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
-   "/home/kyung/21_hf271/RPI/image_transport/install/env.sh")
+   "/home/nvidia/21_hf271/image_transport/install/env.sh")
   if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
   if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
-file(INSTALL DESTINATION "/home/kyung/21_hf271/RPI/image_transport/install" TYPE PROGRAM FILES "/home/kyung/21_hf271/RPI/image_transport/build/catkin_generated/installspace/env.sh")
+file(INSTALL DESTINATION "/home/nvidia/21_hf271/image_transport/install" TYPE PROGRAM FILES "/home/nvidia/21_hf271/image_transport/build/catkin_generated/installspace/env.sh")
 endif()
 
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
   list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
-   "/home/kyung/21_hf271/RPI/image_transport/install/setup.bash;/home/kyung/21_hf271/RPI/image_transport/install/local_setup.bash")
+   "/home/nvidia/21_hf271/image_transport/install/setup.bash;/home/nvidia/21_hf271/image_transport/install/local_setup.bash")
   if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
   if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
-file(INSTALL DESTINATION "/home/kyung/21_hf271/RPI/image_transport/install" TYPE FILE FILES
-    "/home/kyung/21_hf271/RPI/image_transport/build/catkin_generated/installspace/setup.bash"
-    "/home/kyung/21_hf271/RPI/image_transport/build/catkin_generated/installspace/local_setup.bash"
+file(INSTALL DESTINATION "/home/nvidia/21_hf271/image_transport/install" TYPE FILE FILES
+    "/home/nvidia/21_hf271/image_transport/build/catkin_generated/installspace/setup.bash"
+    "/home/nvidia/21_hf271/image_transport/build/catkin_generated/installspace/local_setup.bash"
     )
 endif()
 
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
   list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
-   "/home/kyung/21_hf271/RPI/image_transport/install/setup.sh;/home/kyung/21_hf271/RPI/image_transport/install/local_setup.sh")
+   "/home/nvidia/21_hf271/image_transport/install/setup.sh;/home/nvidia/21_hf271/image_transport/install/local_setup.sh")
   if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
   if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
-file(INSTALL DESTINATION "/home/kyung/21_hf271/RPI/image_transport/install" TYPE FILE FILES
-    "/home/kyung/21_hf271/RPI/image_transport/build/catkin_generated/installspace/setup.sh"
-    "/home/kyung/21_hf271/RPI/image_transport/build/catkin_generated/installspace/local_setup.sh"
+file(INSTALL DESTINATION "/home/nvidia/21_hf271/image_transport/install" TYPE FILE FILES
+    "/home/nvidia/21_hf271/image_transport/build/catkin_generated/installspace/setup.sh"
+    "/home/nvidia/21_hf271/image_transport/build/catkin_generated/installspace/local_setup.sh"
     )
 endif()
 
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
   list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
-   "/home/kyung/21_hf271/RPI/image_transport/install/setup.zsh;/home/kyung/21_hf271/RPI/image_transport/install/local_setup.zsh")
+   "/home/nvidia/21_hf271/image_transport/install/setup.zsh;/home/nvidia/21_hf271/image_transport/install/local_setup.zsh")
   if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
   if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
-file(INSTALL DESTINATION "/home/kyung/21_hf271/RPI/image_transport/install" TYPE FILE FILES
-    "/home/kyung/21_hf271/RPI/image_transport/build/catkin_generated/installspace/setup.zsh"
-    "/home/kyung/21_hf271/RPI/image_transport/build/catkin_generated/installspace/local_setup.zsh"
+file(INSTALL DESTINATION "/home/nvidia/21_hf271/image_transport/install" TYPE FILE FILES
+    "/home/nvidia/21_hf271/image_transport/build/catkin_generated/installspace/setup.zsh"
+    "/home/nvidia/21_hf271/image_transport/build/catkin_generated/installspace/local_setup.zsh"
     )
 endif()
 
-if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
   list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
-   "/home/kyung/21_hf271/RPI/image_transport/install/.rosinstall")
+   "/home/nvidia/21_hf271/image_transport/install/.rosinstall")
   if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
   if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
     message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
   endif()
-file(INSTALL DESTINATION "/home/kyung/21_hf271/RPI/image_transport/install" TYPE FILE FILES "/home/kyung/21_hf271/RPI/image_transport/build/catkin_generated/installspace/.rosinstall")
+file(INSTALL DESTINATION "/home/nvidia/21_hf271/image_transport/install" TYPE FILE FILES "/home/nvidia/21_hf271/image_transport/build/catkin_generated/installspace/.rosinstall")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/home/kyung/21_hf271/RPI/image_transport/build/gtest/cmake_install.cmake")
-  include("/home/kyung/21_hf271/RPI/image_transport/build/cv_bridge/cmake_install.cmake")
-  include("/home/kyung/21_hf271/RPI/image_transport/build/image_transport/cmake_install.cmake")
-  include("/home/kyung/21_hf271/RPI/image_transport/build/webcam/cmake_install.cmake")
+  include("/home/nvidia/21_hf271/image_transport/build/gtest/cmake_install.cmake")
+  include("/home/nvidia/21_hf271/image_transport/build/cv_bridge/cmake_install.cmake")
+  include("/home/nvidia/21_hf271/image_transport/build/image_transport/cmake_install.cmake")
+  include("/home/nvidia/21_hf271/image_transport/build/webcam/cmake_install.cmake")
 
 endif()
 
@@ -145,5 +140,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/kyung/21_hf271/RPI/image_transport/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/nvidia/21_hf271/image_transport/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
