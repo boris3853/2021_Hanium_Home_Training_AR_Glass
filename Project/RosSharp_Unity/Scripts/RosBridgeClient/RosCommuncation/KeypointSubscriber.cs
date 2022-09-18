deccb370591ab49f6453e1fbfc8baeda274ec673
Kyung <boris3853@naver.com>
2022-09-18 19:51:05 +0900

    Updated File Dir

---
+++ Project/RosSharp_Unity/Scripts/RosBridgeClient/RosCommuncation/KeypointSubscriber.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace RosSharp.RosBridgeClient
+{
+    public class KeypointSubscriber : UnitySubscriber<MessageTypes.Keypoints.Keypoints> 
+    {
+
+        public double[] x;
+        public double[] y;
+        public double[] prob;
+        public double[] KeyTrue;
+        public byte type_ex;
+
+        protected override void Start()
+        {
+            base.Start();
+        }
+
+
+        protected override void ReceiveMessage(MessageTypes.Keypoints.Keypoints message)
+        {
+            x = message.x;
+            y = message.y;
+            prob = message.prob;
+            KeyTrue = message.KeyTrue;
+            type_ex = message.type_ex;
+        }
+    }
+    
+}
